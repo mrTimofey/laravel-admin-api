3cc6f6e989968b91a9c4b64f113564e95e0c7f62
timoxa <timoxa17@gmail.com>
2017-12-01 14:21:31 +0300

    wysiwyg image resize

--- src/Http/Controllers/Wysiwyg.php
+++ src/Http/Controllers/Wysiwyg.php
@@ -4,15 +4,23 @@ namespace MrTimofey\LaravelAdminApi\Http\Controllers;
 
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
+use Intervention\Image\ImageManager;
 use MrTimofey\LaravelAioImages\ImageModel as Image;
 
 class Wysiwyg extends Base
 {
-    public function upload() {
+    public function upload(ImageManager $intervention) {
         $funcNum = $this->req->get('CKEditorFuncNum');
         try {
             $this->req->validate(['upload' => ['image', 'max:4096']]);
             $image = Image::upload($this->req->file('upload'), ['wysiwyg' => true, 'ext' => $file->getClientOriginalExtension()]);
+            $size = config('api_admin.wysiwyg.image_upload_size');
+            if ($size && count($size) > 0) {
+                $target = $image->getAbsPath();
+                $interventionImage = $intervention->make($target);
+                $interventionImage->fit($size[0], $size[1] ?? null);
+                $interventionImage->save($target);
+            }
             return '<script>window.parent.CKEDITOR.tools.callFunction(\'' . $funcNum . '\', \'' . $image->getPath() . '\');</script>';
         }
         catch (ValidationException $e) {
