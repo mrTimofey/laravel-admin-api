1d7b3ba7a2b7715c10e2b8f1ae4e1baed9bdf882
timoxa <timoxa17@gmail.com>
2017-12-01 14:13:56 +0300

    oops

--- src/Http/Controllers/Meta.php
+++ src/Http/Controllers/Meta.php
@@ -20,7 +20,7 @@ class Meta extends Base
             'nav' => config('admin_api.nav'),
             'entities' => $this->resolver->getMeta(),
             'wysiwyg' => config('admin_api.wysiwyg'),
-            'upload' => config('admin_api.upload')
+            'upload_path' => config('admin_api.upload.public_path')
         ]);
     }
 }
